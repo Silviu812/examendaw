102850479156535aa128e28788639b569a302d68
Silviu812 <silviuandrei1056@gmail.com>
2024-01-24 21:20:38 +0200

    first commit

---
+++ app-api/app-api/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace app_api
+{
+    public class User
+    {
+        public int Id { get; set; }
+        //SA FAC ASTA SA FIE UNIC
+        public string UserName { get; set; } = String.Empty;
+        public string EMail { get; set; } = String.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
---
+++ app-api/app-api/UserDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace app_api
+{
+    public class UserDBContext : DbContext
+    {
+        public UserDBContext(DbContextOptions<UserDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; } = null!;
+
+    }
+}
